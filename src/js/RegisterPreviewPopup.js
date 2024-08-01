89d32849a1549cb117073e1567de325283712162
kymngan1003 <kymngan1003@gmail.com>
2024-08-01 13:41:52 +0700

    uninstall some none use package

--- src/js/RegisterPreviewPopup.js
+++ src/js/RegisterPreviewPopup.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 import '../scss/Common.scss';
 import '../scss/RegisterPopup.scss';
 import RegisterCheckPopup from "./RegisterCheckPopup";
